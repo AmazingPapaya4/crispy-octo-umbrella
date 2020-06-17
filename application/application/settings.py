930873389e2332dafb231262571e413b8db419a0
Taro Kawada <amazing.papaya.4@gmail.com>
2020-06-17 16:32:56 +0900

    update settings (to redirect 404 to react)

--- application/application/settings.py
+++ application/application/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uvy5*l=n51@@_#i77bm2u56h5tks!^$sulk-#u84)+&nga6cr-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
