ac731ae23970ba457938329586e4646f1e7c007c
Taro Kawada <amazing.papaya.4@gmail.com>
2020-06-17 21:13:49 +0900

    deployment files

--- application/application/base.py
+++ application/application/base.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uvy5*l=n51@@_#i77bm2u56h5tks!^$sulk-#u84)+&nga6cr-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
